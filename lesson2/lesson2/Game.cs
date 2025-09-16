2d4e7665f1688df32b87c6c522d7dfe02353c8f5
tali926 <Hand1e10@mbjcomp.org.il>
2025-09-16 12:51:06 +0300

    Add files via upload

---
+++ lesson2/lesson2/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    public class Game:Item
+    {
+        public string name {  get; set; }
+        public int id { get; set; }
+        public string type {  get; set; }
+
+
+        public Game(int quantity, int price, string name, int id, string type) : base(quantity, price)
+        {
+            this.name = name;
+            this.id = id;
+            this.type = type;
+
+           
+
+        }
+    }
+}
